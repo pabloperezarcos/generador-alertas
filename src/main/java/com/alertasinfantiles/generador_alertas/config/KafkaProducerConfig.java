c81a89715df83f26a9f5dea3ac9004fc6df1f936
pabloperezarcos <pabloperezarcos@gmail.com>
2025-02-15 16:47:57 -0300

    Update KafkaProducerConfig.java

--- src/main/java/com/alertasinfantiles/generador_alertas/config/KafkaProducerConfig.java
+++ src/main/java/com/alertasinfantiles/generador_alertas/config/KafkaProducerConfig.java
@@ -1,13 +1,16 @@
 package com.alertasinfantiles.generador_alertas.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +21,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092,kafka-2:9093,kafka-3:9094");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
@@ -28,5 +31,13 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic createTopic() {
+        return TopicBuilder.name("alertas_infantiles")
+                .partitions(3) // Define la cantidad de particiones
+                .replicas(1) // Define la cantidad de réplicas (ajusta según necesidad)
+                .build();
     }
 }
